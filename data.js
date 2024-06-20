c002ee174d73d845f343475443506237ffffdbe7
mindongun23 <93426133+mindongun123@users.noreply.github.com>
2024-06-20 22:22:30 +0700

    Update data.js

--- data.js
+++ data.js
@@ -1,1 +1,29 @@
 
+{
+    "questions": [
+        {
+            "question": "25+17=?",
+            "answer": 42,
+            "explanation": "Để tính tổng của 25 và 17, bạn cộng hai số lại với nhau: 25 + 17 = 42.",
+            "difficulty": 1
+        },
+        {
+            "question": "13+48=?",
+            "answer": 35,
+            "explanation": "Để trừ 13 từ 48, bạn trừ 13 từ 48: 48 - 13 = 35.",
+            "difficulty": 1
+        },
+        {
+            "question": "8x6=?",
+            "answer": 48,
+            "explanation": "Để nhân 8 với 6, bạn nhân hai số lại với nhau: 8 * 6 = 48.",
+            "difficulty": 1
+        },
+        {
+            "question": "36:4=?",
+            "answer": 9,
+            "explanation": "Để chia 36 cho 4, bạn chia 36 cho 4: 36 / 4 = 9.",
+            "difficulty": 1
+        }
+    ]
+}
